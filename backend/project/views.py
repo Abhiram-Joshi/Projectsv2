ff4d51b367591b286e02ee07a6663a4e8c509718
Abhiram-Joshi <abramjoshi@gmail.com>
2021-08-25 19:52:53 +0530

    fix: removed auth middleware

--- backend/project/views.py
+++ backend/project/views.py
@@ -16,50 +16,50 @@ class ProjectDataAPIView(APIView):
 
     def get(self, request):
 
-        if request.user.role == "admin":
+        # if request.user.role == "admin":
 
-            model_data = ProjectModel.objects.values()
-            field_names = [field.name for field in ProjectModel._meta.get_fields()]
-            repo_field_names = list(filter(lambda s: s.startswith("repo"), field_names))
-            repo_field_names.remove("repo_contributors")
+        model_data = ProjectModel.objects.values()
+        field_names = [field.name for field in ProjectModel._meta.get_fields()]
+        repo_field_names = list(filter(lambda s: s.startswith("repo"), field_names))
+        repo_field_names.remove("repo_contributors")
 
-            data = []
-            
-            for instance_data in model_data:
-                repo_data = dict()
+        data = []
+        
+        for instance_data in model_data:
+            repo_data = dict()
 
-                for i in repo_field_names:
-                    repo_data[i] = instance_data[i]
+            for i in repo_field_names:
+                repo_data[i] = instance_data[i]
 
-                repo_data["repo_contributors"] = ProjectModel.objects.get(repo_name=instance_data["repo_name"]).repo_contributors.values()
+            repo_data["repo_contributors"] = ProjectModel.objects.get(repo_name=instance_data["repo_name"]).repo_contributors.values()
 
-                data.append(repo_data)
+            data.append(repo_data)
 
-            return Response(data, status=status.HTTP_200_OK)
+        return Response(data, status=status.HTTP_200_OK)
 
-        else:
-            response = {"message": "User not authenticated"}
-            return Response(response, status=status.HTTP_403_FORBIDDEN)
+        # else:
+        #     response = {"message": "User not authenticated"}
+        #     return Response(response, status=status.HTTP_403_FORBIDDEN)
 
 
 class ProjectIssuesAPIView(APIView):
     # permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        if request.user.role == "admin":
-            serializer = GetRepoSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            repo_name = serializer.validated_data["repo_name"]
-            repo_data = dict()
+        # if request.user.role == "admin":
+        serializer = GetRepoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        repo_name = serializer.validated_data["repo_name"]
+        repo_data = dict()
 
-            # Number of issues of the repo
-            repo_data = utilities.count_issues(repo_name)
+        # Number of issues of the repo
+        repo_data = utilities.count_issues(repo_name)
 
-            return Response(repo_data, status=status.HTTP_200_OK)
+        return Response(repo_data, status=status.HTTP_200_OK)
 
-        else:
-            response = {"message": "User not authenticated"}
-            return Response(response, status=status.HTTP_403_FORBIDDEN)
+        # else:
+        #     response = {"message": "User not authenticated"}
+        #     return Response(response, status=status.HTTP_403_FORBIDDEN)
 
 
 class ProjectContributorsAPIView(APIView):
@@ -67,18 +67,18 @@ class ProjectContributorsAPIView(APIView):
 
     def get(self, request):
         
-        if request.user.role == "admin":
-            serializer = GetRepoSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            repo_name = serializer.validated_data["repo_name"]
+        # if request.user.role == "admin":
+        serializer = GetRepoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        repo_name = serializer.validated_data["repo_name"]
 
-            repo_data = utilities.get_contributors(repo_name)
+        repo_data = utilities.get_contributors(repo_name)
 
-            return Response(repo_data, status=status.HTTP_200_OK)
+        return Response(repo_data, status=status.HTTP_200_OK)
 
-        else:
-            response = {"message": "User not authenticated"}
-            return Response(response, status=status.HTTP_403_FORBIDDEN)
+        # else:
+        #     response = {"message": "User not authenticated"}
+        #     return Response(response, status=status.HTTP_403_FORBIDDEN)
 
 
 class ProjectLanguagesAPIView(APIView):
@@ -86,18 +86,18 @@ class ProjectLanguagesAPIView(APIView):
 
     def get(self, request):
 
-        if request.user.role == "admin":
-            serializer = GetRepoSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            repo_name = serializer.validated_data["repo_name"]
+        # if request.user.role == "admin":
+        serializer = GetRepoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        repo_name = serializer.validated_data["repo_name"]
 
-            repo_data = utilities.get_languages(repo_name)
+        repo_data = utilities.get_languages(repo_name)
 
-            return Response(repo_data, status=status.HTTP_200_OK)
+        return Response(repo_data, status=status.HTTP_200_OK)
 
-        else:
-            response = {"message": "User not authenticated"}
-            return Response(response, status=status.HTTP_403_FORBIDDEN)
+        # else:
+        #     response = {"message": "User not authenticated"}
+        #     return Response(response, status=status.HTTP_403_FORBIDDEN)
 
 
 class ProjectPullRequestsAPIView(APIView):
@@ -105,15 +105,15 @@ class ProjectPullRequestsAPIView(APIView):
 
     def get(self, request):
         
-        if request.user.role == "admin":
-            serializer = GetRepoSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            repo_name = serializer.validated_data["repo_name"]
+        # if request.user.role == "admin":
+        serializer = GetRepoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        repo_name = serializer.validated_data["repo_name"]
 
-            repo_data = utilities.count_pull_requests(repo_name)
+        repo_data = utilities.count_pull_requests(repo_name)
 
-            return Response(repo_data, status=status.HTTP_200_OK)
+        return Response(repo_data, status=status.HTTP_200_OK)
 
-        else:
-            response = {"message": "User not authenticated"}
-            return Response(response, status=status.HTTP_403_FORBIDDEN)
+        # else:
+        #     response = {"message": "User not authenticated"}
+        #     return Response(response, status=status.HTTP_403_FORBIDDEN)
